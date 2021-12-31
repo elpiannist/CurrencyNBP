dfff222e09784a248aecd739814d306b5cfecd19
elpiannist <chopin.tercet@gmail.com>
2021-12-31 15:12:36 +0100

    added validation, cleaned some mess

--- src/Service/NBPIntegrator.php
+++ src/Service/NBPIntegrator.php
@@ -3,6 +3,7 @@ namespace App\Service;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpClient\CurlHttpClient;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use App\Repository\CurrencyRepository;
 use App\Entity\Currency;
 
@@ -12,21 +13,28 @@ class NBPIntegrator
 
     private CurlHttpClient $httpClient;
     private EntityManagerInterface $em;
+    private ValidatorInterface $validator;
 
-    public function __construct(CurrencyRepository $currencyRepository, EntityManagerInterface $em)
+    public function __construct(CurrencyRepository $currencyRepository, EntityManagerInterface $em, ValidatorInterface $validator)
     {
         $this->currencyRepository = $currencyRepository;
         $this->httpClient = new CurlHttpClient();
         $this->em = $em;
+        $this->validator = $validator;
     }
-    public function fetchCurrencies($table = 'A', $field = 'mid') : string
+    public function fetchCurrencies($table = 'A', $field = 'mid')
     {
         $URL = self::API_URL. '/tables/'. $table;
         $response = $this->httpClient->request("GET", $URL, ['headers'=>['Accept'=>'application-json']])->getContent();
         $data  = json_decode($response, true);
-        var_dump($data);
-        foreach ($data[0]['rates'] as $responseCurrency)
+        $currencies = $data[0]['rates'];
+        foreach ($currencies as $responseCurrency)
         {
+            if (! $this->array_keys_exists(['code', 'currency', $field], $responseCurrency))
+            {
+                return false;
+            }
+
             if ( is_null ($currency = $this->currencyRepository->findByCurrencyCode($responseCurrency['code'] )))
             {
                 $currency = new Currency();
@@ -38,9 +46,21 @@ class NBPIntegrator
             {
                 $currency->setExchangeRate($responseCurrency['mid']);
             }
+
+            $errors = $this->validator->validate($currency);
+            
+            if (count($errors) > 0 )
+            {
+                return false;
+            }
+
             $this->em->persist($currency);
         }
         $this->em->flush();
-        return "asd";
+        return true;
+    }
+
+    private function array_keys_exists(array $keys, array $arr) {
+        return !array_diff_key(array_flip($keys), $arr);
     }
 }
